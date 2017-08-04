244e3ce960cc4f7e471e3571db5e2144f5d8e209
Bryan Apperson <me@bryanapperson.com>
2017-08-04 13:56:41 -0400

    feat: add sample data for tests and some logic

---
+++ stanford_algorithms/count_split_inversions.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def file_to_list(fname):
+    """Read a file and return a list newlines stripped and casted to int.
+    
+    :param fname: name of the file to read"""
+    with open(fname) as f:
+        content = [int(x.strip('\n')) for x in f.readlines()]
+    return content
+
+
+def count_split_inversions(count_array):
+    """Count the split inversions in an array/list."""
+    return 1
+
+
+if __name__ == '__main__':
+    try:
+        contents = file_to_list(sys.argv[1])
+    except KeyError:
+        print('This script requires one file to read as an argument.')
+        sys.exit(1)
--- tests/test_count_split_inversions.py
+++ tests/test_count_split_inversions.py
@@ -1,7 +1,17 @@
-"""Tests for stanford_algorithms.karatsuba."""
+"""Tests for stanford_algorithms.count_split_inversions."""
+from os.path import join
+from os import getcwd
 from stanford_algorithms import count_split_inversions
+
+
+def test_file_to_list():
+    contents = count_split_inversions.file_to_list(
+        join(getcwd(), 'sample_data/integer_array.txt'))
+    assert len(contents) == 100000
 
 
 def test_count_split_inversions():
     """Test for the info function."""
-    pass
+    contents = count_split_inversions.file_to_list(
+        join(getcwd(), 'sample_data/integer_array.txt'))
+    assert count_split_inversions.count_split_inversions(contents) == 1
